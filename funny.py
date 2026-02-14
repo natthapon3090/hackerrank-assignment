5a32e8701b66d06e5c4ae3435e65ec151642d6f4
natthapon3090 <garfield11kp@gmail.com>
2026-02-14 22:23:39 +0700

    Update funny.py

--- funny.py
+++ funny.py
@@ -1,1 +1,12 @@
+def funny_string(s: str) -> str:
+    reversed_s = s[::-1]
+
+    for i in range(1, len(s)):
+        diff1 = abs(ord(s[i]) - ord(s[i-1]))
+        diff2 = abs(ord(reversed_s[i]) - ord(reversed_s[i-1]))
+
+        if diff1 != diff2:
+            return "Not Funny"
+
+    return "Funny"
 
