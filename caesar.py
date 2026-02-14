9898d6574c25c5b6bf62cd992ae8b0e57d4ada02
natthapon3090 <garfield11kp@gmail.com>
2026-02-14 22:53:17 +0700

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,1 +1,14 @@
+def caesar_cipher(s: str, k: int) -> str:
+    result = ""
+    k = k % 26
+
+    for char in s:
+        if char.isupper():
+            result += chr((ord(char) - ord('A') + k) % 26 + ord('A'))
+        elif char.islower():
+            result += chr((ord(char) - ord('a') + k) % 26 + ord('a'))
+        else:
+            result += char
+
+    return result
 
