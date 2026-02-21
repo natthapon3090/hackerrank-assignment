95bf29ced1fc1d2372afdd0a8171b4724ca9d764
natthapon3090 <garfield11kp@gmail.com>
2026-02-21 16:36:32 +0700

    Update test_two_characters.py

--- test_two_characters.py
+++ test_two_characters.py
@@ -1,1 +1,15 @@
+import unittest
+from two_characters import two_characters
 
+
+class TestTwo(unittest.TestCase):
+
+    def test_case1(self):
+        self.assertEqual(two_characters("beabeefeab"),5)
+
+    def test_case2(self):
+        self.assertEqual(two_characters("aaaa"),0)
+
+
+if __name__=="__main__":
+    unittest.main()
