4a235400ea6d990b538a3e04b86615fda8c6d584
natthapon3090 <garfield11kp@gmail.com>
2026-02-21 16:36:07 +0700

    Update test_caesar.py

--- test_caesar.py
+++ test_caesar.py
@@ -1,1 +1,18 @@
+import unittest
+from caesar_cipher import caesar_cipher
 
+
+class TestCaesar(unittest.TestCase):
+
+    def test_lower(self):
+        self.assertEqual(caesar_cipher("abc",2),"cde")
+
+    def test_upper(self):
+        self.assertEqual(caesar_cipher("XYZ",2),"ZAB")
+
+    def test_symbol(self):
+        self.assertEqual(caesar_cipher("abc-123",2),"cde-123")
+
+
+if __name__=="__main__":
+    unittest.main()
