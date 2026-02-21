8c30c297fa057140563c9f73773b69465f3953f4
natthapon3090 <garfield11kp@gmail.com>
2026-02-21 16:36:47 +0700

    Update test_grid.py

--- test_grid.py
+++ test_grid.py
@@ -1,1 +1,15 @@
+import unittest
+from grid_challenge import grid_challenge
 
+
+class TestGrid(unittest.TestCase):
+
+    def test_yes(self):
+        self.assertEqual(grid_challenge(["abc","ade","efg"]),"YES")
+
+    def test_no(self):
+        self.assertEqual(grid_challenge(["cba","daf","ghi"]),"NO")
+
+
+if __name__=="__main__":
+    unittest.main()
