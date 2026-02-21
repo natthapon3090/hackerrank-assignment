c754f23496b11c9878b263190fda0844994f7e9f
natthapon3090 <garfield11kp@gmail.com>
2026-02-21 16:35:44 +0700

    Update test_alternating.py

--- test_alternating.py
+++ test_alternating.py
@@ -1,1 +1,18 @@
+import unittest
+from alternating_characters import alternating_characters
 
+
+class TestAlternating(unittest.TestCase):
+
+    def test_case1(self):
+        self.assertEqual(alternating_characters("AAAA"),3)
+
+    def test_case2(self):
+        self.assertEqual(alternating_characters("ABABAB"),0)
+
+    def test_case3(self):
+        self.assertEqual(alternating_characters("AABBAB"),2)
+
+
+if __name__=="__main__":
+    unittest.main()
