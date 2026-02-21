8ef95bbcf5dcddbd03310e47524d30527770eb19
natthapon3090 <garfield11kp@gmail.com>
2026-02-21 16:37:48 +0700

    Update two_characters.py

--- two_characters.py
+++ two_characters.py
@@ -1,1 +1,24 @@
+def two_characters(s):
 
+    best=0
+
+    chars=set(s)
+
+    for a in chars:
+        for b in chars:
+
+            if a!=b:
+
+                t=[c for c in s if c==a or c==b]
+
+                valid=True
+
+                for i in range(1,len(t)):
+
+                    if t[i]==t[i-1]:
+                        valid=False
+
+                if valid:
+                    best=max(best,len(t))
+
+    return best
